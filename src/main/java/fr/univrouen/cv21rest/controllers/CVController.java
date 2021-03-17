8a02f6609ac8c1e3dc0e254a9181631edfcc4c4b
Titouan CORNILLEAU <cornitit@inf-35-13.dptinfo.univ-rouen.fr>
2021-03-17 12:23:23 +0100

    ajout controller

--- src/main/java/fr/univrouen/cv21rest/controllers/CVController.java
+++ src/main/java/fr/univrouen/cv21rest/controllers/CVController.java
@@ -2,19 +2,25 @@ package fr.univrouen.cv21rest.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class CVController {
 	@GetMapping("/resume")
 	public String getListCVinXML() {
 		return"Envoi de la liste des CV";
 	}
+	
 	@GetMapping("/id")
-	public String getCVinXML() {
-		return"Détail du CV demandé";
-	}
-	/*@GetMapping("/id")
 	public String getCVinXML(
 		@RequestParam(value = "name") String texte) {
 		return ("Détail du CV demandé : " + texte);
-	}*/
+	}
+	@GetMapping("/test")
+	public String gettestXML(
+		@RequestParam(value = "nb") String texte, @RequestParam(value = "texte") String texte2) {
+		return ("Test : </br> id = " + texte+"</br> titre = " + texte2);
+	}
+	
+	
 }
