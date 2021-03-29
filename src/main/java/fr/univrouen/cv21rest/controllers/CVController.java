7d61b0faee357cf001f924b975fd11540633de6d
Titouan CORNILLEAU <cornitit@inf-35-14.dptinfo.univ-rouen.fr>
2021-03-29 11:40:07 +0200

    modofication cv controller

--- src/main/java/fr/univrouen/cv21rest/controllers/CVController.java
+++ src/main/java/fr/univrouen/cv21rest/controllers/CVController.java
@@ -6,21 +6,47 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CVController {
+	@GetMapping("/help")
+	public String helpXML() {
+		return"explique comment que ça marche";
+	}
 	@GetMapping("/resume")
-	public String getListCVinXML() {
-		return"Envoi de la liste des CV";
+	public String resumeXML() {
+		return"coucou resume";
 	}
 	
-	@GetMapping("/id")
-	public String getCVinXML(
-		@RequestParam(value = "name") String texte) {
-		return ("Détail du CV demandé : " + texte);
+//	@GetMapping("/id")
+//	public String getCVinXML(
+//		@RequestParam(value = "name") String texte) {
+//		return ("Détail du CV demandé : " + texte);
+//	}
+//	@GetMapping("/test")
+//	public String gettestXML(
+//		@RequestParam(value = "nb") String texte, @RequestParam(value = "texte") String texte2) {
+//		return ("Test : </br> id = " + texte+"</br> titre = " + texte2);
+//	}
+	
+
+	@GetMapping("/")
+	public String accueilXML() {
+		String page = "";
+		
+		page += "<!DOCTYPE html>";
+		page += "<html>";
+		page += "<head>";
+		page += "<title>Projet web XML</title>";
+		page += "</head>";
+		page += "<body>";
+		page += "<h1>Projet Web XML</h1>";
+		page += "<p>v0.1</br>Lassire Eden & Cornilleau Titouan</p>";
+		//page += "<img src=\"/images/Universite_de_Rouen.jpg\"></img>";
+		page += "</body>";
+		page += "</html>";
+		
+		return page ;
 	}
-	@GetMapping("/test")
-	public String gettestXML(
-		@RequestParam(value = "nb") String texte, @RequestParam(value = "texte") String texte2) {
-		return ("Test : </br> id = " + texte+"</br> titre = " + texte2);
-	}
+		
+		
 	
 	
 }
--- src/main/java/fr/univrouen/cv21rest/controllers/IndexController.java
+++ src/main/java/fr/univrouen/cv21rest/controllers/IndexController.java
@@ -6,8 +6,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController 
 public class IndexController {
 
-	@GetMapping("/")
-	public String index() {
-		return"Hello cv21 !";
-	}
+	
 }
