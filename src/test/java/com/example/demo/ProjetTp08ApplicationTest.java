bcc7d732fd0c4eab8eef03a9af66da9562add748
cornitit <80394461+cornitit@users.noreply.github.com>
2021-04-29 22:55:04 +0200

    Update ProjetTp08ApplicationTest.java

--- src/test/java/com/example/demo/ProjetTp08ApplicationTest.java
+++ src/test/java/com/example/demo/ProjetTp08ApplicationTest.java
@@ -2,8 +2,9 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import fr.univrouen.cv21.ProjetTp08Application;
 
-@SpringBootTest
+@SpringBootTest(classes=ProjetTp08Application.class)
 class ProjetTp08ApplicationTests {
 
 	@Test
